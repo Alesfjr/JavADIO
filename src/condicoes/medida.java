591c5122047ad6b6489240a74184bf11fc682b14
Alesfjr <aalexfurtadodev@gmail.com>
2025-03-22 15:52:53 -0300

    .

---
+++ src/condicoes/medida.java
@@ -0,0 +1,20 @@
+package condicoes;
+
+public class medida {
+    public static void main(String[] args) {
+
+        String size = "X";
+
+        switch (size) {
+            case "P": {
+                System.out.println("Pequeno");
+                break;
+            }
+            case "X": {
+                System.out.println("Grande");
+                break;
+            }
+        }
+
+    }
+}
--- src/condicoes/swich.java
+++
@@ -1,4 +0,0 @@
-package condicoes;
-
-public class swich {
-}
