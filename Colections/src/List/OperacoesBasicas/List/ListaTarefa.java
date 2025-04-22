471ebfe96251dcc54ad6da5f58fc0140801edb5b
Alesfjr <aalexfurtadodev@gmail.com>
2025-04-22 20:37:19 -0300

    .

---
+++ Colections/src/List/OperacoesBasicas/List/ListaTarefa.java
@@ -0,0 +1,49 @@
+package List.OperacoesBasicas.List;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaTarefa {
+
+    //atributos
+    private List<Tarefa> TarefaList;
+
+    public ListaTarefa(){
+        this.TarefaList = new ArrayList<>();
+    }
+    public void adcionartarfa (String descricao){
+        TarefaList.add(new Tarefa(descricao));
+
+    }
+    public void removertarefa(String descricao){
+        List<Tarefa> tarefasparaRemover = new ArrayList<>();
+        for (Tarefa t : TarefaList){
+            if(t.getDescricao().equalsIgnoreCase(descricao)){
+                tarefasparaRemover.add(t);
+
+            }
+        }
+        TarefaList.removeAll(tarefasparaRemover);
+
+    }
+    public  int obterNumerosTotalTarefas(){
+        return TarefaList.size();
+
+    }
+    public void obterDescricoaoTerefas(){
+        System.out.println(TarefaList);
+    }
+
+    public static void main(String[] args) {
+        ListaTarefa listaTarefa = new ListaTarefa();
+        System.out.println("O numero total de elementos na lista é: "+listaTarefa.obterNumerosTotalTarefas());
+        listaTarefa.adcionartarfa("Tarefa1");
+        listaTarefa.adcionartarfa("Tarefa3");
+        listaTarefa.adcionartarfa("Tarefa2");
+        listaTarefa.adcionartarfa("Tarefa3");
+        listaTarefa.removertarefa("Tarefa3");
+        System.out.println("O numero total de elementos na lista é: "+listaTarefa.obterNumerosTotalTarefas());
+        listaTarefa.obterDescricoaoTerefas();
+    }
+
+}
---
+++ Colections/src/List/OperacoesBasicas/Listex2/Carinho.java
@@ -0,0 +1,66 @@
+package List.OperacoesBasicas.Listex2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Carinho  {
+
+
+    private List<Item> CarinhoCompras;
+
+    public Carinho(){
+        this.CarinhoCompras= new ArrayList<>();
+
+    }
+
+    public void adicionarItem(String nome, double preco, int quantidade){
+
+        CarinhoCompras.add(new Item(nome,preco,quantidade));
+
+    }
+    public void removerItem(String nome, double preco, int quantidade) {
+        List<Item> itensParaRemover = new ArrayList<>();
+        for (Item item : CarinhoCompras) {
+            if (item.getNome().equalsIgnoreCase(nome)
+                    && item.getPreco() == preco
+                    && item.getQuantidade() == quantidade) {
+                itensParaRemover.add(item);
+            }
+        }
+        CarinhoCompras.removeAll(itensParaRemover);
+    }
+        public double calcularValorTotal(){
+            double total = 0.0;
+            for (Item item:CarinhoCompras){
+                total+= item.getPreco()* item.getQuantidade();
+
+        }
+            return total;
+
+        }
+    public void exibirItens(){
+        for (Item item: CarinhoCompras){
+            System.out.println("Item: " + item.getNome() +
+                " | Preço: R$" + item.getPreco() +
+                " | Quantidade: " + item.getQuantidade());
+        }
+    }
+
+    public static void main(String[] args) {
+        Carinho carinho =new Carinho();
+
+        carinho.adicionarItem("arroz",33.0,2);
+        carinho.adicionarItem("feijao",15.0,3);
+        carinho.adicionarItem("iorgut",3.0,10);
+        carinho.adicionarItem("oleo",9.50,2);
+        System.out.println("Produtos no carinho: " );
+        carinho.removerItem("arroz",33.0,1);
+        carinho.exibirItens();
+
+        System.out.println("Valor total:" );
+        double total = carinho.calcularValorTotal();
+        System.out.println(total);
+
+
+    }
+}
